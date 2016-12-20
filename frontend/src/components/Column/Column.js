11736dc0f4e6913ff8ce838486d524a35d7d7ccc
davidwells <david@inboundnow.com>
2016-12-19 19:36:24 -0800

    render live data

--- frontend/src/components/Column/Column.js
+++ frontend/src/components/Column/Column.js
@@ -8,18 +8,28 @@ export default class Column extends Component {
     super(props)
   }
   renderItems() {
-    const {count} = this.props
-    let items = []
-    for (var i = 0; i < count; i++) {
-      items[i] = (
-        <li className="drag-item">
+    const { items } = this.props
+    if(!items) {
+      return null
+    }
+    return items.map((item, i) => {
+      let milestone = item.milestone.title
+      if (item.milestone && item.milestone.title ) {
+        milestone = (
           <span className="drag-item-text">
-            API Gateway -> Kinesis Firehose - New Line Issue
+            {item.milestone.title}
           </span>
+        )
+      }
+      return(
+        <li key={i} className="drag-item">
+          <span className="drag-item-text">
+            {item.title}
+          </span>
+          {milestone}
         </li>
       )
-    }
-    return items
+    })
   }
   render () {
     const { title } = this.props
@@ -27,7 +37,7 @@ export default class Column extends Component {
       <li className="drag-column drag-column-in-progress">
   			<span className="drag-column-header">
   				<h2>{title}</h2>
-          <svg class="drag-header-more" data-target="options2" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24">
+          <svg className="drag-header-more" data-target="options2" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24">
             <path d="M0 0h24v24H0z" fill="none"></path>
             <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
           </svg>
