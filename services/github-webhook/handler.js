d13100ba354897e89cffe277bfa34022556b6a40
davidwells <david@inboundnow.com>
2017-01-22 17:05:36 -0800

    clean up code

--- services/github-webhook/handler.js
+++ services/github-webhook/handler.js
@@ -1,238 +1,266 @@
 const crypto = require('crypto')
 const syncRequest = require('sync-request')
 const db = require('./db')
-
-function signRequestBody(key, body) {
-  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
-}
+const DEBUG = process.env.DEBUG
+const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME || 'serverlessbot-2'
+const CLOSED_ITEMS_TABLE = process.env.CLOSED_ITEMS_TABLE
+const OPEN_ITEMS_TABLE = process.env.OPEN_ITEMS_TABLE
 
 module.exports.githubWebhookListener = (event, context, callback) => {
+  // console.log(event)
   var errMsg
-  const token = process.env.GITHUB_WEBHOOK_SECRET
-  // console.log('token', token)
+  const body = JSON.parse(event.body)
   const headers = event.headers
-  // console.log('headers', headers)
-  const sig = headers['X-Hub-Signature']
+  const signature = headers['X-Hub-Signature']
   const githubEvent = headers['X-GitHub-Event']
   const id = headers['X-GitHub-Delivery']
-  const calculatedSig = signRequestBody(token, JSON.stringify(event.body))
-  // console.log(event)
-  if (typeof token !== 'string') {
+  const webhookToken = process.env.GITHUB_WEBHOOK_SECRET
+  const calculatedSignature = signRequestBody(webhookToken, event.body)
+
+  if (DEBUG) {
+    console.log('headers', headers)
+    console.log('body', body)
+    console.log('X-Hub-Signature: ', signature)
+    console.log('calculatedSignature: ', calculatedSignature)
+  }
+
+  // Return early on Github 'ping' events for webhook test
+  if (body.action === 'ping') {
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        pong: true,
+      })
+    })
+  }
+
+  // Validate webtoken exists
+  if (!webhookToken || typeof webhookToken !== 'string') {
     errMsg = '[401] must provide a \'GITHUB_WEBHOOK_SECRET\' env variable'
     return callback(new Error(errMsg))
   }
 
-  if (!sig) {
+  // Validate signed request from Github
+  if (!signature) {
     errMsg = '[401] No X-Hub-Signature found on request'
     return callback(new Error(errMsg))
   }
 
+  // Validate event from Github
   if (!githubEvent) {
     errMsg = '[422] No X-Github-Event found on request'
     return callback(new Error(errMsg))
   }
 
+  // Validate delivery id from Github
   if (!id) {
     errMsg = '[401] No X-Github-Delivery found on request'
     return callback(new Error(errMsg))
   }
-  // console.log('sig', sig)
-  // console.log('calculatedSig', calculatedSig)
-  if (sig !== calculatedSig) {
+
+  if (signature !== calculatedSignature) {
     errMsg = '[401] X-Hub-Signature incorrect. Github webhook token doesn\'t match'
     return callback(new Error(errMsg))
   }
 
-  var data = {
-    // webhook_id: id, // webhook X-Github-Delivery uid
-    // event: githubEvent, // webhook X-Github-Event
-    // action: event.body.action
-    // body: event.body,
-  }
-  // console.log('event.body', event.body)
-  var issue = event.body.issue
-  const isIssue = issue && !issue.pull_request
-  const isPullRequest = event.body.pull_request || issue && issue.pull_request
+  // Validation check out. Begin processing event
   console.log('---------------------------------')
-  console.log(`Github-Event: "${githubEvent}" with action: "${event.body.action}"`)
+  console.log(`Github-Event: "${githubEvent}"`)
+  console.log(`Github-Action: "${body.action}"`)
   console.log('---------------------------------')
-  // Handle Issue logic
-  if (isIssue) {
-    data.type = 'issue'
-  } else if (isPullRequest) {
-    // Handle PR logic
-    data.type = 'pull-request'
-    const prData = (issue) ? issue.pull_request : event.body.pull_request
-    const issueURL = prData.url.replace(/\/pulls\//, '/issues/')
 
-    const githubAPItoken = process.env.GITHUB_API_TOKEN
-    const issueRequest = syncRequest('GET', issueURL, {
+  console.log('action', body.action)
+  console.log('event.body', body)
+
+  var issue = body.issue
+  const isIssue = issue && !issue.pull_request
+  const isPullRequest = body.pull_request || issue && issue.pull_request
+  console.log('isPullRequest', isPullRequest)
+  if (!issue && !isPullRequest) {
+    console.log('Exiting early event not an issue or PR')
+    console.log('---------------------------------')
+    console.log(`Github-Event: "${githubEvent}"`)
+    console.log(`Github-Action: "${body.action}"`)
+    console.log('---------------------------------')
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'event is not an Issue or PR',
+      })
+    })
+  }
+
+  var type = 'issue'
+  if (isPullRequest) {
+    // Pull request require an addition API call for label data
+    type = 'pull-request'
+    const prData = (issue) ? issue.pull_request : body.pull_request
+    const issueURL = prData.url.replace(/\/pulls\//, '/issues/')
+    var githubRequestHeaders = {
       'headers': {
-        'User-Agent': 'serverlessbot-2',
-        'Authorization': `token ${githubAPItoken}`
+        'User-Agent': GITHUB_USERNAME,
       }
-    });
+    }
+    if (GITHUB_API_TOKEN) {
+      githubRequestHeaders.headers['Authorization'] = `token ${GITHUB_API_TOKEN}`
+    }
+    const issueRequest = syncRequest('GET', issueURL, githubRequestHeaders)
     try {
-      issue = JSON.parse(issueRequest.getBody('utf8'));
+      // Set issue data from PR
+      issue = JSON.parse(issueRequest.getBody('utf8'))
     } catch (e) {
       errMsg = '[422] Github JSON malformed'
       return callback(new Error(errMsg))
     }
-    // console.log('pr object', prData)
   }
-  //console.log('issue object', issue)
 
-  // set data to store to DB
-  data.number = issue.number
-  data.title = issue.title
-  data.comments = issue.comments
-  data.created_at = issue.created_at
-  data.updated_at = issue.updated_at
-  // data.id = issue.id
-  // data.state = issue.state
-  // data.html_url = issue.html_url
-  // data.milestone = issue.milestone
-
+  // set default labels. Default: []
+  var labels = issue.labels
   if (issue.labels && issue.labels.length) {
-    console.log('has labels')
-    const labelArray = issue.labels.map(function(label, i) {
+    labels = issue.labels.map((label) => {
+      if (DEBUG) {
+        console.log(`Label: ${label.name}`, label)
+      }
       return {
         id: label.id,
         name: label.name,
         color: label.color
       }
     })
-    data.labels = labelArray
-  } else {
-    console.log('no labels found')
-    data.labels = issue.labels
   }
 
+  // set default labels. Default: []
+  var assignees = []
   if (issue.assignees && issue.assignees.length) {
-    console.log('has assignees')
-    const assigneeArray = issue.assignees.map(function(assignee, i) {
+    assignees = issue.assignees.map((assignee, i) => {
+      if (DEBUG) {
+        console.log('assigneeData', assignee)
+      }
       return {
         login: assignee.login,
         id: assignee.id,
       }
     })
-    data.assignees = assigneeArray
-  } else {
-    data.assignees = []
   }
 
+  // set default milestone. Default: {}
+  var milestone = {}
   if (issue.milestone) {
-    data.milestone = {
+    milestone = {
       number: issue.milestone.number,
       title: issue.milestone.title,
     }
-  } else {
-    console.log('no milestone')
-    data.milestone = {}
   }
 
-  console.log('data to update', data)
+  // set data to store to DB
+  const item = {
+    type: type,
+    number: issue.number,
+    title: issue.title,
+    comments: issue.comments,
+    // store time as Unix
+    updated_at: new Date(issue.created_at).getTime(),
+    created_at: new Date(issue.updated_at).getTime(),
+    // store additional issue info
+    milestone: milestone,
+    assignees: assignees,
+    labels: labels,
+    //id: issue.id
+    //state: issue.state
+  }
+
+  if (DEBUG) {
+    console.log('item', item)
+  }
 
   if (issue.state === 'closed' || issue.closed_at) {
     console.log('Issue or PR closed. Remove from DB')
-    var deleteParams = {
-      TableName: 'status_board_open_issues',
+    db.delete({
+      TableName: OPEN_ITEMS_TABLE,
       Key: {
-        number: data.number
+        number: item.number
       },
-    };
-    db.delete(deleteParams, function(err, deletedResp) {
-      if (err) {
-        callback(err);
-      }
-      console.log('item deleted from status_board_open_issues', deletedResp)
+    }, (err, resp) => {
+      if (err) return callback(err)
+      console.log(`item ${item.number} deleted from ${OPEN_ITEMS_TABLE} Table`, resp)
       db.put({
-        TableName: 'status_board_closed_issues',
-        Item: data
-      }, function(err, putResponse) {
-        if (err) {
-          callback(err);
-        }
-        console.log('item added to status_board_closed_issues', putResponse)
-        const success = {
+        TableName: CLOSED_ITEMS_TABLE,
+        Item: item
+      }, function(error, response) {
+        if (error) return callback(error)
+        console.log(`item ${item.number} added to ${CLOSED_ITEMS_TABLE} Table`, response)
+        callback(null, {
           statusCode: 200,
           body: JSON.stringify({
             input: event,
           }),
-        };
-        callback(null, success);
-      });
-    });
-    // return false
-    // remove issue.id
+        })
+      })
+    })
   } else {
     // add or update item in database
-    var params = {
-      TableName: 'status_board_open_issues',
-      Item: data
-    };
-
-    db.put(params, function(err, putResponse) {
-      if (err) {
-        callback(err);
-      }
-      console.log('item added to status_board_open_issues')
-      // check if in closed table and delete if so
+    db.put({
+      TableName: OPEN_ITEMS_TABLE,
+      Item: item
+    }, (err, resp) => {
+      if (err) return callback(err)
+      console.log(`item ${item.number} added to ${OPEN_ITEMS_TABLE} Table`, resp)
+      // Delete from closed table if it's being reopened
       db.delete({
-        TableName: 'status_board_closed_issues',
+        TableName: CLOSED_ITEMS_TABLE,
         Key: {
-          number: data.number
+          number: item.number
         }
-      }, function(err, deletedResp) {
-        if (err) {
-          callback(err);
-        }
-        console.log('item deleted from status_board_closed_issues')
-        const addedResponse = {
+      }, function(err, response) {
+        if (err) return callback(err)
+        console.log(`item ${item.number} deleted from ${CLOSED_ITEMS_TABLE} Table`, response)
+        return callback(null, {
           statusCode: 200,
           body: JSON.stringify({
             input: event,
-          }),
-        };
-        callback(null, addedResponse);
+          })
+        })
       })
-    });
+    })
   }
-};
+}
 
+function signRequestBody(key, body) {
+  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
+}
 
 module.exports.getItems = (event, context, callback) => {
   db.scan({
-    TableName: 'status_board_open_issues',
+    TableName: OPEN_ITEMS_TABLE,
   }, function(err, items) {
-    if (err) {
-      callback(err);
-    }
-    console.log(items)
-    const addedResponse = {
+    if (err) return callback(err)
+    if (DEBUG) console.log(items)
+
+    return callback(null, {
       statusCode: 200,
       body: JSON.stringify({
         items: items,
-      }),
-    };
-    callback(null, addedResponse);
+      })
+    })
   })
 }
 
 module.exports.getCompletedItems = (event, context, callback) => {
+  // https://github.com/acantril/aCloudGuru-DynamoDB/blob/master/ZZ-DataModelv3/datamodelv3.py#L201-L205
   db.scan({
-    TableName: 'status_board_closed_issues',
+    TableName: CLOSED_ITEMS_TABLE,
   }, function(err, items) {
-    if (err) {
-      callback(err);
-    }
-    console.log(items)
-    const addedResponse = {
+    if (err) return callback(err)
+    if (DEBUG) console.log(items)
+
+    // sort by date range
+
+    return callback(null,  {
       statusCode: 200,
       body: JSON.stringify({
         items: items,
       }),
-    };
-    callback(null, addedResponse);
+    })
   })
 }
